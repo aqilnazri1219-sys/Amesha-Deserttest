ad9e008208bd8eb2633bd703b568d6ad83523eee
aqilnazri1219-sys <aqilnazri1219@gmail.com>
2026-06-11 22:58:26 +0800

    Implement user login functionality with Firebase

---
+++ app.js
@@ -0,0 +1,30 @@
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-auth.js";
+
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", async () => {
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+
+    await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    alert("Login berjaya");
+
+  } catch (error) {
+
+    alert(error.message);
+
+  }
+
+});
