d7ad752352b78712e21c4c39f692399d9176c43c
aqilnazri1219-sys <aqilnazri1219@gmail.com>
2026-06-12 09:19:58 +0800

    Refactor authentication functions and UI elements

--- auth.js
+++ auth.js
@@ -9,13 +9,13 @@ const goToRegister = document.getElementById('go-to-register');
 const goToLogin = document.getElementById('go-to-login');
 
 // Fungsi tukar paparan borang (Toggle UI)
-goToRegister.addEventListener('click', (e) => {
+goToRegister.addEventListener('click', () => {
     e.preventDefault();
     loginForm.classList.add('hidden');
     registerForm.classList.remove('hidden');
 });
 
-goToLogin.addEventListener('click', (e) => {
+goToLogin.addEventListener('click', () => {
     e.preventDefault();
     registerForm.classList.add('hidden');
     loginForm.classList.remove('hidden');
